3a46f32d0a66518d66eae8d3b3835ec3aa7f1ca6
Samuel Fry <samuel.fry13@gmail.com>
2024-10-23 20:34:36 -0400

    initial commit with file picker

---
+++ src/lib/Dropzone.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import { getCurrentWebview } from "@tauri-apps/api/webview";
+  import Upload from "lucide-svelte/icons/upload";
+  import { onDestroy } from "svelte";
+  import Button from "./components/ui/button/button.svelte";
+  import { cn } from "./utils";
+  import { open } from "@tauri-apps/plugin-dialog";
+
+  let dragging = $state(false);
+  let error = $state("");
+
+  const unlisten = getCurrentWebview().onDragDropEvent((event) => {
+    if (event.payload.type === "enter") {
+      dragging = true;
+      if (event.payload.paths.length > 1) error = "Only one video file allowed";
+    } else if (event.payload.type === "drop") {
+      dragging = false;
+      if (event.payload.paths.length > 1) {
+        error = "Only one video file allowed";
+        setTimeout(() => {
+          error = "";
+        }, 1000);
+      }
+    } else if (event.payload.type === "leave") {
+      error = "";
+      dragging = false;
+    }
+  });
+
+  onDestroy(() => {
+    unlisten.then((fn) => fn());
+  });
+</script>
+
+<div
+  class={cn(
+    "w-full max-w-md p-8 text-center border-2 border-dashed rounded-lg transition-colors duration-300 ease-in-out",
+    dragging && !error
+      ? "border-primary bg-primary/10"
+      : "border-gray-300 bg-white",
+    error && "border-red-700 bg-red-100"
+  )}
+>
+  <Upload class="mx-auto text-gray-400 w-12 h-12 mb-4" />
+  <h2 class="text-xl font-semibold mb-2">
+    {#if error}
+      {error}
+    {:else}
+      Drop your video here
+    {/if}
+  </h2>
+  <p class="text-sm text-gray-500 mb-4">or click to select files</p>
+  <input type="file" id="fileInput" class="sr-only" />
+  <label for="fileInput">
+    <Button
+      onclick={() => {
+        open({
+          multiple: false,
+          filters: [
+            {
+              name: "Videos",
+              extensions: ["mp4"],
+            },
+          ],
+        }).then((file) => {
+          if (!file) return;
+
+          console.log("Chosen file", file);
+        });
+      }}>Select Files</Button
+    >
+  </label>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Dropzone from "$lib/Dropzone.svelte";
+</script>
+
+<main class="flex items-center justify-center min-h-screen bg-gray-100">
+  <Dropzone />
+</main>
